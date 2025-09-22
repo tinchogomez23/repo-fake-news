034acdcfcf668ca96890bed1941995218defcbb6
tinchogomez23 <23gmartin@gmail.com>
2025-09-22 19:42:50 -0300

    Corrección 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import MiCompPolitica from "./MiCompPolitica";
+import MiCompEntretenimiento from "./MiCompEntretenimiento";
 
 import "./index.css";
 
 const App = () => (
   <div className="container">
-     <MiCompPolitica />
+     <MiCompEntretenimiento />
   </div>
 );
 ReactDOM.render(<App />, document.getElementById("app"));
