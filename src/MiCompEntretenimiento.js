cc374ae6b4ce119d8d55b5db673afd4d66ecfe05
tinchogomez23 <23gmartin@gmail.com>
2025-09-22 20:00:11 -0300

    Agregado 1

--- src/MiCompEntretenimiento.js
+++ src/MiCompEntretenimiento.js
@@ -3,10 +3,10 @@ import React from "react";
 function MiCompEntretenimiento() {
     // Retornar una noticia politica o del tema asignado, como un parrafo de texto.
     return <div>
-       <p> Esta es una noticia de entretenimiento. Walt Disney está vivo.</p>
-
-        
-
+       <h3>🎬 Noticias de Entretenimiento</h3>
+       <p><strong>NOTICIA FALSA:</strong> Walt Disney está vivo y trabajando en secreto desde una instalación criogénica en los estudios de Disney.</p>
+       <p>Según fuentes no confirmadas, el legendario animador habría sido preservado criogénicamente desde 1966 y continuaría dirigiendo las decisiones creativas de la compañía desde las sombras.</p>
+       <p><em>Fecha: 22 de septiembre, 2025 | Fuente: Rumores de Internet</em></p>
     </div>;
 }
 
